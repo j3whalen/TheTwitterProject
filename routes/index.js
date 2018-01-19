c6b845d7d66916330f830d7df04a55027b8f6090
Josh Whalen <j3whalen@student.bridgew.edu>
2018-01-18 19:45:14 -0500

    Added Async/Await and now sending all necessary information to implement graph

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var sentiment = require('sentiment');
 var express = require('express');
 var Twitter = require('twitter');
 var router = express.Router();
@@ -7,28 +8,29 @@ var client = new Twitter({
   access_token_key: '945822571982917632-NV2nlQcAzg8eViItlsaPdnliXoUmoow',
   access_token_secret: 'beV5qIzCYewBsWTMkaTAGEqS7gXcu5WvgjsX3rFwPBrEX'
 });
-/*_______________NOTES______________
 
-+++++Google API will not be able to work++++++
-for each query we are going to have 1,400 requests sent to google api, and 
-after 5,000 requests we will have to start paying.
-
-GOOD NEWS
-IBM has a tone analyzer which actually might be better
-it has 7 tones example (Sad,Anger,Sympathy) along with an asscociated rating(0.76)
-We can really grab the emotions from these tweets we just need to find a way
-to store the data and display it in a effective way.
-
-If we use IBM API do we really need to be doing two separate queries against each other?
-Like how will we display all the different parts of information. Especially from two separate queries
-*/ 
-
-//-----------------------------------------------------------------------
 //Global Variables
 
 var dates = getArrayOfLastWeekDates();
-//tweets that we will be getting back will be BEFORE the date passed as a parameter
-//var total = 250; //amount of tweeets we want per day Dont use yet
+var Query1Tweets = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+];
+var Query2Tweets = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  []
+];
+
 router.get('/', function (req, res, next) {
   res.status(200).render('index', {
     title: 'The Twitter Project'
@@ -36,88 +38,71 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/search', function (req, res, next) {
-  var UserQuery1 = req.body.search;
+  var UserQuery1 = req.body.search1;
   var UserQuery2 = req.body.search2;
-  var Query1Tweets = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-  ];
-  var Query2Tweets = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-  ];
+  var queries = [];
+  queries.push(UserQuery1);
+  queries.push(UserQuery2);
 
-
-  //tweets from first search field
-  // console.log("_______________________________________________________________________________________");
-  // console.log("_____OUR DATES_____");
-  // console.log("date[0] ", dates[0]);
-  // console.log("date[1] ", dates[1]);
-  // console.log("date[2] ", dates[2]);
-  // console.log("date[3] ", dates[3]);
-  // console.log("date[4] ", dates[4]);
-  // console.log("date[5] ", dates[5]);
-  // console.log("date[6] ", dates[6]);
-  // console.log("___________________");
-  // console.log("");
-  // console.log("");
-  // console.log("");
-
-// get100Tweets(UserQuery1, dates[0], function(results){// TEST FUNCTION TO CALL GETTWEETS ONCE
-//   console.log("completed get100tweets");
-// });
-
-  dates.forEach(function (date, index) {
-    get100Tweets(UserQuery1, date, function (results) {
-      Query1Tweets[index] = Query1Tweets[index].concat(results);
-      console.log("day ", index, " Date ",results[0]);
-      console.log(Query1Tweets[index]);
+  GetTweetsAndComputeSentiment(queries, dates).then((results) => {
+    res.render('result', {
+      dates: dates,
+      results1: results[0],
+      results2: results[1]
     });
   });
-
-  res.render('result', {});
-  // get100Tweets(UserQuery1, total, day, Query1Tweets, function (result) {
-  //   res.status(200).render('result', {
-  //     tweets: result
-  //   });
-  // });
-
 });
 
-
-function get100Tweets(query, day, callback) {
-  var listoftweets = [];
-  client.get('search/tweets', {
-    q: query,
-    tweet_mode: "extended",
-    count: 100,
-    until: day,
-    exclude: "retweets",
-    lang: "en"
-  }, function (error, tweets, response) {
-    if (!error) {
-      for (var i = 0; i < tweets.statuses.length; i++) {
-        listoftweets.push(tweets.statuses[i].full_text); //text
-        //listoftweets.push(tweets.statuses[i].created_at); //Testing to see if the dates are correct
+async function GetTweetsAndComputeSentiment(queries, dates) {
+  var sentiment1 = [];
+  var sentiment2 = []
+  for(var j = 0; j < 2; j++){
+    for (var i = 0; i < dates.length; i++) {
+      var tweets = await get100Tweets(queries[j], dates[i]);
+      var sentiment = await getSentiment(tweets);
+      if(j === 0){
+        sentiment1.push(sentiment);
       }
-
-      callback(listoftweets);
-    } else {
-      //res.status(500).json({ error: error });
-      console.log("error getting tweets");
+      else{
+        sentiment2.push(sentiment);
+      }
     }
-  });
+  }
+  return [sentiment1, sentiment2];
+}
 
+function getSentiment(tweets) {
+  return new Promise(resolve => {
+    var sum = 0;
+    for (var i = 0; i < tweets.length; i++) {
+      sum = sum + sentiment(tweets[i]).comparative;
+    }
+    resolve(sum / tweets.length);
+  });
+}
+
+function get100Tweets(query, day) {
+  return new Promise(resolve => {
+    console.log(query);
+    var listoftweets = [];
+    client.get('search/tweets', {
+      q: query,
+      tweet_mode: "extended",
+      count: 100,
+      until: day,
+      exclude: "retweets",
+      lang: "en"
+    }, function (error, tweets, response) {
+      if (!error) {
+        for (var i = 0; i < tweets.statuses.length; i++) {
+          listoftweets.push(tweets.statuses[i].full_text); //text
+        }
+        resolve(listoftweets);
+      } else {
+        console.log("error getting tweets");
+      }
+    });
+  });
 }
 
 function getArrayOfLastWeekDates() {
@@ -127,6 +112,7 @@ function getArrayOfLastWeekDates() {
     var x = new Date(date.setDate(date.getDate() - i));
     arrayOfTheWeek.push(x.toISOString().split('T')[0]);
   }
+  console.log("Days of the week",arrayOfTheWeek);
   return arrayOfTheWeek;
 }
 
